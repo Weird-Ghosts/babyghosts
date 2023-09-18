cc2402262014262bf9cd17dac601859b2c8a1b38
Jennie Robinson Faber <jennie@jenniefaber.com>
2023-09-18 13:16:40 -0400

    Update to image processing

--- components/content/Card.vue
+++ components/content/Card.vue
@@ -15,8 +15,26 @@ defineProps({
 </script>
 
 <template>
-  <div class="md:flex items-start align-items-start mt-8 rounded-xl card">
+  <div class="md:flex items-start align-items-start mt-8 card">
     <div class="md:w-1/3 mr-4 headshot">
+      <NuxtImg
+        :src="path"
+        provider="imgix"
+        width="300"
+        :alt="alt"
+        height="500"
+        fit="cover"
+        :modifiers="{
+          auto: 'format,compress',
+          crop: 'faces',
+          duotoneAlpha: 100,
+          blendWidth: 500,
+          blendFit: 'scale',
+          blendAlpha: 50,
+          blend: 'https://www.datocms-assets.com/35480/1601833062-10.png',
+          duotone: '161616,f4f4f5',
+        }"
+      />
       <NuxtImg
         :src="path"
         provider="imgix"
@@ -29,9 +47,9 @@ defineProps({
           auto: 'format,compress',
           crop: 'faces',
           duotoneAlpha: 100,
-          blendWidth: 1000,
+          blendWidth: 500,
           blendFit: 'scale',
-          blendAlpha: 100,
+          blendAlpha: 50,
           blend: 'https://babyghosts.fund/img/bg-pink.png',
           duotone: '0b0b0b,f4f4f5',
         }"
