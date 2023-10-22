0c0e8010cdf1efbecac3fb6f94d3e9fa81faaa88
Jennie Robinson Faber <jennie@jenniefaber.com>
2023-10-22 15:57:59 -0400

    Add robots; optimize logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,42 +1,20 @@
 <template>
   <div class="text-crt text-7xl">
     <NuxtLink to="/">
-      <NuxtImg src="/img/logo.png" alt="logo" class="logo" />
+      <NuxtImg
+        src="/img/logo.png"
+        provider="imgix"
+        class="logo"
+        width="417"
+        height="218"
+        alt="Baby Ghosts logo, glowing text with a CRT pattern"
+        :modifiers="{
+          auto: 'format,compress',
+        }"
+      />
     </NuxtLink>
   </div>
 </template>
-<script setup>
-// import { gsap } from "gsap";
-// import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
-// import { ref, onMounted } from "vue";
-
-// gsap.registerPlugin(MorphSVGPlugin);
-
-// // const characters = ref(["S2", "T", "S", "O", "H", "G", "Y", "B2", "A", "B"]);
-// const characters = ref(["B", "A", "Y"]);
-
-// function animateMorph(character) {
-//   gsap.to(`#${character}_reg`, {
-//     morphSVG: `#${character}_puff`,
-//     duration: 2,
-//     repeat: -1, // for infinite loop
-//     yoyo: true, // to morph back and forth
-//     ease: "power1.inOut",
-//   });
-// }
-
-// function shuffle(array) {
-//   array.sort(() => Math.random() - 0.5);
-// }
-
-// shuffle(characters.value);
-
-// onMounted(() => {
-//   characters.value.forEach((character) => {
-//     gsap.delayedCall(Math.random() * 5, animateMorph, [character]);
-//   });
-// });
-</script>
 
 <style scoped>
 .logo {
