5f38ccfcdd6aa23a2e30bee3df145063a2b5cd4d
Jennie Robinson Faber <jennie@jenniefaber.com>
2023-10-24 11:12:34 -0400

    Tweaks for FFx

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <div class="grain"></div>
+    <div class="grain-wrapper">
+      <div class="grain"></div>
+    </div>
     <Navigation />
     <main class="w-full mx-auto max-w-screen-xl" role="main">
       <NuxtPage />
